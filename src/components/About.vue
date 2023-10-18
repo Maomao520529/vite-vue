12bc02ea0549fcce39804923a06841d77bd374b4
yenying <maomao520244@gmail.com>
2023-10-18 20:25:58 +0800

    update

--- src/components/About.vue
+++ src/components/About.vue
@@ -1,46 +1,62 @@
 <!-- eslint-disable vue/multi-word-component-names -->
 <template>
     <div class="about  my-5 ">
+        <h1 class="text-center mt-5">Figma 作品</h1>
+        <div class="row row-cols-md-3 g-4 my-3 mt-3">
+            <div class="col d-flex justify-content-center">
+                <div class="card text-center border-light" style="width: 18rem;">
+                    <img src="../assets/玻璃擬態頁設計.png" class="card-img-top" alt="玻璃擬態頁設計" v-viewer>
+                    <div class="card-body ">
+                        <a href="https://www.figma.com/community/file/1296324320137825490" target="_blank" class="text-dark text-decoration-none">玻璃擬態頁設計👆</a>
+                    </div>
+                </div>
+            </div>
+            <!-- <div class="col  d-flex justify-content-center">
+                <div class="card" style="width: 18rem;">
+                    <img src="../assets/花好月圓(櫻花Fly).png" class="card-img-top" alt="花好月圓" v-viewer>
+                </div>
+            </div> -->
+        </div>
         <h1 class="text-center mt-5">海報製作</h1>
         <div class="row row-cols-md-3 g-4 my-3 mt-3">
-            <div class="col  d-flex justify-content-center">
-                <div class="card" style="width: 18rem;">
+            <div class="col d-flex justify-content-center">
+                <div class="card border-light" style="width: 18rem;">
                     <img src="../assets/jeju.png" class="card-img-top" alt="濟洲島" v-viewer>
                 </div>
             </div>
-            <div class="col  d-flex justify-content-center">
-                <div class="card" style="width: 18rem;">
+            <div class="col d-flex justify-content-center">
+                <div class="card border-light" style="width: 18rem;">
                     <img src="../assets/城堡-白天-黑夜.gif" class="card-img-top" alt="城堡-白天-黑夜" v-viewer>
                 </div>
             </div>
         </div>
         <h1 class="text-center mt-5">Banner 製作</h1>
         <div class="row row-cols-md-3 g-4 my-3 mt-3">
-            <div class="col  d-flex justify-content-center ">
-                <div class="card" style="width: 18rem;">
+            <div class="col d-flex justify-content-center ">
+                <div class="card border-light" style="width: 18rem;">
                     <img src="../assets/萬聖節(櫻花).png" class="card-img-top" alt="萬聖節(櫻花)" v-viewer>
                 </div>
             </div>
-            <div class="col  d-flex justify-content-center">
-                <div class="card" style="width: 18rem;">
+            <div class="col d-flex justify-content-center">
+                <div class="card border-light" style="width: 18rem;">
                     <img src="../assets/halloween.png" class="card-img-top" alt="萬聖節(家具)" v-viewer>
                 </div>
             </div>
-            <div class="col  d-flex justify-content-center">
-                <div class="card" style="width: 18rem;">
+            <div class="col d-flex justify-content-center">
+                <div class="card border-light" style="width: 18rem;">
                     <img src="../assets/farm.png" class="card-img-top" alt="茭白筍" v-viewer>
                 </div>
             </div>
         </div>
         <h1 class="text-center mt-5">小作品</h1>
         <div class="row row-cols-md-3 g-4 my-3 mt-3">
-            <div class="col  d-flex justify-content-center">
-                <div class="card" style="width: 18rem;">
+            <div class="col d-flex justify-content-center">
+                <div class="card border-light" style="width: 18rem;">
                     <img src="../assets/郵戳章信封.png" class="card-img-top" alt="郵戳章信封" v-viewer>
                 </div>
             </div>
-            <div class="col  d-flex justify-content-center">
-                <div class="card" style="width: 18rem;">
+            <div class="col d-flex justify-content-center">
+                <div class="card border-light" style="width: 18rem;">
                     <img src="../assets/花好月圓(櫻花Fly).png" class="card-img-top" alt="花好月圓" v-viewer>
                 </div>
             </div>
