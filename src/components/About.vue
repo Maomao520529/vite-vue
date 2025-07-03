ff70c3424b471c3f3c150b62e31d768cbd68b6ad
Maomao520529 <50698535+Maomao520529@users.noreply.github.com>
2025-07-03 22:49:09 +0800

    0703update

--- src/components/About.vue
+++ src/components/About.vue
@@ -5,7 +5,7 @@
         <div class="row row-cols-md-3 g-4 my-3 mt-3">
             <div class="col d-flex justify-content-center">
                 <div class="card text-center border-light" style="width: 18rem;">
-                    <img src="../assets/玻璃擬態頁設計.png" class="card-img-top" alt="玻璃擬態頁設計" v-viewer>
+                    <img src="../assets/1-1.png" class="card-img-top" alt="玻璃擬態頁設計" v-viewer>
                     <div class="card-body ">
                         <a href="https://www.figma.com/community/file/1296324320137825490" target="_blank" class="text-dark text-decoration-none">玻璃擬態頁設計👆</a>
                     </div>
@@ -13,7 +13,7 @@
             </div>
             <div class="col  d-flex justify-content-center">
                 <div class="card text-center border-light" style="width: 18rem;">
-                    <img src="../assets/ART擬態頁設計.jpg" class="card-img-top" alt="ART擬態頁設計" v-viewer>
+                    <img src="../assets/1-2.jpg" class="card-img-top" alt="ART擬態頁設計" v-viewer>
                     <div class="card-body ">
                         <a href="https://www.figma.com/community/file/1296481888531950151/art" target="_blank" class="text-dark text-decoration-none">ART 擬態頁設計👆</a>
                     </div>
@@ -24,12 +24,27 @@
         <div class="row row-cols-md-3 g-4 my-3 mt-3">
             <div class="col d-flex justify-content-center">
                 <div class="card border-light" style="width: 18rem;">
-                    <img src="../assets/jeju.png" class="card-img-top" alt="濟洲島" v-viewer>
+                    <img src="../assets/2-1.png" class="card-img-top" alt="濟洲島" v-viewer>
                 </div>
             </div>
             <div class="col d-flex justify-content-center">
                 <div class="card border-light" style="width: 18rem;">
-                    <img src="../assets/城堡-白天-黑夜.gif" class="card-img-top" alt="城堡-白天-黑夜" v-viewer>
+                    <img src="../assets/2-2.gif" class="card-img-top" alt="城堡-白天-黑夜" v-viewer>
+                </div>
+            </div>
+             <div class="col d-flex justify-content-center">
+                <div class="card border-light" style="width: 18rem;">
+                    <img src="../assets/2-3.png" class="card-img-top" alt="靠枕海報" v-viewer>
+                </div>
+            </div>
+             <div class="col d-flex justify-content-center">
+                <div class="card border-light" style="width: 18rem;">
+                    <img src="../assets/2-4.png" class="card-img-top" alt="靠枕海報0520" v-viewer>
+                </div>
+            </div>
+            <div class="col d-flex justify-content-center">
+                <div class="card border-light" style="width: 18rem;">
+                    <img src="../assets/2-5.png" class="card-img-top" alt="懷舊時光" v-viewer>
                 </div>
             </div>
         </div>
@@ -37,17 +52,17 @@
         <div class="row row-cols-md-3 g-4 my-3 mt-3">
             <div class="col d-flex justify-content-center ">
                 <div class="card border-light" style="width: 18rem;">
-                    <img src="../assets/萬聖節(櫻花).png" class="card-img-top" alt="萬聖節(櫻花)" v-viewer>
+                    <img src="../assets/3-1.png" class="card-img-top" alt="萬聖節(櫻花)" v-viewer>
                 </div>
             </div>
             <div class="col d-flex justify-content-center">
                 <div class="card border-light" style="width: 18rem;">
-                    <img src="../assets/halloween.png" class="card-img-top" alt="萬聖節(家具)" v-viewer>
+                    <img src="../assets/3-2.png" class="card-img-top" alt="萬聖節(家具)" v-viewer>
                 </div>
             </div>
             <div class="col d-flex justify-content-center">
                 <div class="card border-light" style="width: 18rem;">
-                    <img src="../assets/farm.png" class="card-img-top" alt="茭白筍" v-viewer>
+                    <img src="../assets/3-3.png" class="card-img-top" alt="茭白筍" v-viewer>
                 </div>
             </div>
         </div>
@@ -55,12 +70,12 @@
         <div class="row row-cols-md-3 g-4 my-3 mt-3">
             <div class="col d-flex justify-content-center">
                 <div class="card border-light" style="width: 18rem;">
-                    <img src="../assets/郵戳章信封.png" class="card-img-top" alt="郵戳章信封" v-viewer>
+                    <img src="../assets/4-1.png" class="card-img-top" alt="郵戳章信封" v-viewer>
                 </div>
             </div>
             <div class="col d-flex justify-content-center">
                 <div class="card border-light" style="width: 18rem;">
-                    <img src="../assets/花好月圓(櫻花Fly).png" class="card-img-top" alt="花好月圓" v-viewer>
+                    <img src="../assets/4-2.png" class="card-img-top" alt="花好月圓" v-viewer>
                 </div>
             </div>
         </div>
